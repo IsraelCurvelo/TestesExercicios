226884d3bdf2fa2aedc911e4dffdff985ebdaf46
Israel Curvelo <raellcurvelo@gmail.com>
2020-04-17 14:01:18 -0300

    Exercicio de Ordenação de lista

---
+++ TesteComparison/TesteComparison/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteComparison
+{
+    class Produto : IComparable<Produto>
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Preco.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Produto produto)
+        {
+            return Nome.ToUpper().CompareTo(produto.Nome.ToUpper());
+        }
+    }
+}
---
+++ TesteComparison/TesteComparison/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TesteComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> lista = new List<Produto>();
+
+            lista.Add(new Produto("TV", 900.00));
+            lista.Add(new Produto("Notebook", 1200.00));
+            lista.Add(new Produto("Tablet", 450.00));
+
+            lista.Sort();
+
+            foreach (Produto x in lista)
+            {
+                Console.WriteLine(x);
+            }
+
+
+        }
+    }
+}
