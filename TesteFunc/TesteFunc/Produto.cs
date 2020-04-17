b79660f5574b0caa3fb11b781b93006c564f64eb
Israel Curvelo <raellcurvelo@gmail.com>
2020-04-17 19:20:22 -0300

    Func

---
+++ TesteFunc/TesteFunc/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace TesteFunc
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return Nome.ToUpper() + ", " + Preco.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ TesteFunc/TesteFunc/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TesteFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> lista = new List<Produto>();
+
+            lista.Add(new Produto("TV", 900.00));
+            lista.Add(new Produto("Notebook", 4500.00));
+            lista.Add(new Produto("HD", 40.00));
+            lista.Add(new Produto("Mouse", 30.50));
+
+            foreach(Produto x in lista)
+            {
+                Console.WriteLine(x);
+            }
+
+            List<string> novaLista = lista.Select(CaixaAlta).ToList();
+
+            foreach(string x in novaLista)
+            {
+                Console.WriteLine(x);
+            }
+
+
+            Console.WriteLine();
+            Func<Produto, string> func = CaixaAlta;
+            List<string> novaLista2 = lista.Select(func).ToList();
+
+            foreach (string x in novaLista2)
+            {
+                Console.WriteLine(x);
+            }
+
+
+        }
+
+        static string CaixaAlta(Produto p)
+        {
+            return p.Nome.ToUpper();
+        }
+    }
+}
